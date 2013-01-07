61bfb96d09e0d47bbaba2ae0aedaa92b88b95460
Ramesh Sridharan <rameshvs@csail.mit.edu>
2013-01-07 13:24:36 -0700

    fixed example d3 ref and moved TODO items to separate file

--- src/core.js
+++ src/core.js
@@ -1,7 +1,1 @@
-
-// TODO write methods that return CSS selectors for data, to allow for cleaner 
-//      formatting
-// TODO general cleanup
-// TODO make comments jsdoc
-// TODO decide on a design pattern and stick with it (and clean up based on it)
 jsplotlib = {};
